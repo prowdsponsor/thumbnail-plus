890d49b7b28c85aedf719e2d31fac03ef38319f5
Felipe Lessa <felipe.lessa@gmail.com>
2014-03-27 19:32:10 -0300

    Add tests for rejection due to file or image size limits.

--- tests/Main.hs
+++ tests/Main.hs
@@ -13,27 +13,44 @@ import qualified Graphics.ThumbnailPlus as TP
 import qualified Graphics.ThumbnailPlus.ImageSize as TPIS
 
 
+jpn_art :: FilePath
+jpn_art = "tests/data/jpn_art.jpg"
+
+
 main :: IO ()
 main = hspec $ do
   describe "sinkImageInfo" $ do
     it "works for logo.png"    $ check (Just (TP.Size  271   61, TP.PNG)) "tests/data/logo.png"
     it "works for logo.jpg"    $ check (Just (TP.Size  271   61, TP.JPG)) "tests/data/logo.jpg"
     it "works for logo.gif"    $ check (Just (TP.Size  271   61, TP.GIF)) "tests/data/logo.gif"
-    it "works for jpg_art.jpg" $ check (Just (TP.Size 1344 1352, TP.JPG)) "tests/data/jpn_art.jpg"
+    it "works for jpg_art.jpg" $ check (Just (TP.Size 1344 1352, TP.JPG)) jpn_art
     it "rejects invalid file"  $ check Nothing "tests/Main.hs"
   describe "createThumbnails" $ do
+    let conf = def { TP.maxFileSize      = 450239
+                   , TP.maxImageSize     = TP.Size 1344 1352
+                   , TP.reencodeOriginal = TP.NewFileFormat TP.GIF
+                   , TP.thumbnailSizes   = [(TP.Size i i, Nothing) | i <- [3000, 512, 64]]
+                   }
     it "works for jpg_art.jpg" $ do
-      let conf = def { TP.reencodeOriginal = TP.NewFileFormat TP.GIF
-                     , TP.thumbnailSizes   = [(TP.Size i i, Nothing) | i <- [3000, 512, 64]]
-                     }
       fps <- R.runResourceT $ do
-        TP.CreatedThumbnails thumbs _ <- TP.createThumbnails conf "tests/data/jpn_art.jpg"
+        TP.CreatedThumbnails thumbs _ <- TP.createThumbnails conf jpn_art
         liftIO $ do
-          map TP.thumbSize thumbs `shouldBe` [TP.Size 1344 1352, TP.Size 508 512, TP.Size 63 64]
+          map TP.thumbSize thumbs `shouldBe` [TP.maxImageSize conf, TP.Size 508 512, TP.Size 63 64]
           map TP.thumbFormat thumbs `shouldBe` [TP.GIF, TP.JPG, TP.JPG]
           mapM_ checkThumbnail thumbs
         return (map TP.thumbFp thumbs)
       forM_ fps $ \fp -> doesFileExist fp `shouldReturn` False
+    it "rejects due to large file size" $ do
+      let conf' = conf { TP.maxFileSize = TP.maxFileSize conf - 1 }
+      R.runResourceT (TP.createThumbnails conf' jpn_art) `shouldReturn` TP.FileSizeTooLarge (TP.maxFileSize conf)
+    it "rejects due to large image width" $ do
+      -- I should use some lens :).
+      let conf' = conf { TP.maxImageSize = TP.Size 1343 9999 }
+      R.runResourceT (TP.createThumbnails conf' jpn_art) `shouldReturn` TP.ImageSizeTooLarge (TP.maxImageSize conf)
+    it "rejects due to large image height" $ do
+      -- I should use some lens :).
+      let conf' = conf { TP.maxImageSize = TP.Size 9999 1351 }
+      R.runResourceT (TP.createThumbnails conf' jpn_art) `shouldReturn` TP.ImageSizeTooLarge (TP.maxImageSize conf)
 
 check :: Maybe (TP.Size, TP.FileFormat) -> FilePath -> Expectation
 check ex fp = do
